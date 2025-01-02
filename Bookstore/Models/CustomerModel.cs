92375988f81747725b5a346b412310245f50ce90
Bjørn Göttler <bjoern@goettler.dk>
2025-01-02 16:29:49 +0100

    feature: database models: Add Customer Model

--- Bookstore/Models/CustomerModel.cs
+++ Bookstore/Models/CustomerModel.cs
@@ -1,6 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Bookstore.Models;
 
+[Table("customers")]
 public class CustomerModel
 {
-    
+    [Column("id")]
+    public Guid CustomerId { get; set; }
+    [Column("name")]
+    public string Name { get; set; }
+    [Column("created_date")]
+    public DateTime CreatedDate { get; set; }
 }
